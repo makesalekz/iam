c439413d87e58140bf692b67bb2474386ab877eb
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2024-07-29 17:12:51 +0500

    updated settings

--- ent/enum/settings.go
+++ ent/enum/settings.go
@@ -4,11 +4,13 @@ type Settings string
 
 const (
 	TestSetting Settings = "TEST_SETTING"
+	Language    Settings = "LANGUAGE"
+	Theme       Settings = "THEME"
 )
 
 // Values provides list valid values for Enum.
 func (Settings) Values() (kinds []string) {
-	for _, s := range []Settings{TestSetting} {
+	for _, s := range []Settings{TestSetting, Language, Theme} {
 		kinds = append(kinds, string(s))
 	}
 	return
